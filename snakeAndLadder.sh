9604aba1d31ec1e1032a33943d795abdcf960882
abhishekvarmagh <abhishektheverma@gmail.com>
2020-01-30 12:55:19 +0530

    ADDED : Player Reaches Winning Position Using While

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -4,6 +4,7 @@ echo "Welcome to Snake And Ladder Simulator"
 
 NO_OF_PLAYERS=1
 START_POSITION=0
+WINNING_POSITION=100
 NO_PLAY=0
 LADDER=1
 SNAKE=2
@@ -28,9 +29,16 @@ function checkOption()
 			;;
 		$SNAKE)
 			currentPosition=$(( currentPosition-getNumber ))
+			if [ $currentPosition -lt $START_POSITION ]
+			then
+				currentPosition=$START_POSITION
+			fi
 			;;
 	esac
 	echo "Current Position : " $currentPosition
 }
 
-rollingDie
+while [ $currentPosition -le $WINNING_POSITION ]
+do
+	rollingDie
+done
